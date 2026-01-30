027a7f9b9d905c96f8abbf6753bf825fff5590f6
malcom_mathela <malcom_mathela.com>
2026-01-30 13:41:01 +0530

    Routing issues resolved

--- src/main/java/com/example/Spring/Security/JWT/Config/SecurityConfig.java
+++ src/main/java/com/example/Spring/Security/JWT/Config/SecurityConfig.java
@@ -2,16 +2,13 @@ package com.example.Spring.Security.JWT.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -36,7 +33,11 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(authorize ->authorize
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/",
+                                "/index.html",
+                                "/assets/**",
+                                "/favicon.ico",
+                                "/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
---
+++ src/main/java/com/example/Spring/Security/JWT/Controller/FrontendController.java
@@ -0,0 +1,17 @@
+package com.example.Spring.Security.JWT.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FrontendController {
+
+    @GetMapping(value = {
+            "/",
+            "/{path:^(?!api|auth|swagger-ui|v3).*$}",
+            "/**/{path:^(?!api|auth|swagger-ui|v3).*$}"
+    })
+    public String forward() {
+        return "forward:/index.html";
+    }
+}
